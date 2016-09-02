f254027b4dd26ef6bcb376668f220aa1a97bdb3e
AlexanderMac <amatsibarov@gmail.com>
2016-09-02 23:31:43 +0300

    rewrite topbar menu, added sidebar menu

--- client/app/controllers/navbar-controller.js
+++ client/app/controllers/navbar-controller.js
@@ -1,7 +1,8 @@
 'use strict';
 
 angular.module('app').controller('NavbarCtrl', function($scope, $location) {
-  $scope.isActive = function(route, startsWith) {
-    return route === $location.path() || (startsWith && _.startsWith($location.path(), route));
+  $scope.isActive = function(route) {
+    var currentRoute = $location.path();
+    return _.startsWith(currentRoute, route);
   };
 });
