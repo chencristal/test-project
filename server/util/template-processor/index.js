c96d6c21fd9201a5e5a07f96695374d2524413e2
ChenCristal <chencristal2014@gmail.com>
2017-02-21 04:51:22 +0800

    fixed issue of crashing PDF and Word export function

--- server/util/template-processor/index.js
+++ server/util/template-processor/index.js
@@ -37,25 +37,65 @@ handlebars.registerHelper('ifCond', (op, v1, v2, options) => {
 // chen_debug
 //
 handlebars.registerHelper('math', (v1, op, v2) => {
-  /* jshint maxcomplexity: false */
-  switch (op) {
-    case 'add':
-    case 'plus':
-      return v1 + v2;
-    case 'subtract':
-    case 'minus':
-      return v1 - v2;
-    case 'multiply':
-    case 'multiplied by':
-      return v1 * v2;
-    case 'divide':
-    case 'divided by':
-      return v1 / v2;
-    case 'modulus':
-    case 'modulo':
-      return v1 % v2;
-    default:
-      throw new Error('Invalid operator');
+  /* jshint maxcomplexity: false */  
+
+  var parse_date = (param) => {
+    var months = ['January', 'February', 'March', 'April', 'May', 'June',
+      'July', 'August', 'September', 'October', 'November', 'December'];
+    return months[param.getMonth()] + ' ' + 
+      param.getDate() + ', ' + 
+      param.getFullYear();
+  }
+
+  var is_date_operation = false;
+  var date1;
+
+  if (isNaN(parseInt(v1))) {  // chen_debug
+    date1 = new Date(v1);
+    is_date_operation = true;
+  }
+  else {
+    v1 = parseInt(v1);
+  }
+  v2 = parseInt(v2);
+
+  if (is_date_operation) {
+    switch (op) {
+      case 'add':
+      case 'add-date':
+      case 'add-day':
+        date1.setDate(date1.getDate() + v2);
+        return parse_date(date1);
+      case 'add-month':
+        date1.setMonth(date1.getMonth() + v2);
+        return parse_date(date1);
+      case 'add-year':
+        date1.setFullYear(date1.getFullYear() + v2);
+        return parse_date(date1);
+      default:
+        throw new Error('Invalid operator');
+    }
+  } 
+  else {
+    switch (op) {
+      case 'add':
+      case 'plus':
+        return v1 + v2;
+      case 'subtract':
+      case 'minus':
+        return v1 - v2;
+      case 'multiply':
+      case 'multiplied by':
+        return v1 * v2;
+      case 'divide':
+      case 'divided by':
+        return v1 / v2;
+      case 'modulus':
+      case 'modulo':
+        return v1 % v2;
+      default:
+        throw new Error('Invalid operator');
+    }
   }
 });
 
