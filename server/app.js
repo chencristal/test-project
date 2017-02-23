512a9a5109a9a22732c90f694629a99043e259bb
Jason <jlee@celant.com>
2017-02-23 00:23:27 -0500

    setup placeholder for dapp documentation

--- server/app.js
+++ server/app.js
@@ -1,6 +1,7 @@
 'use strict';
 
 var express = require('express');
+var path    = require('path');
 var config  = require('../config/environment');
 var db      = require('./db');
 var log     = require('./util/logger').logger;
@@ -9,6 +10,9 @@ require('./util/promisify');
 require('./util/errors');
 
 var app = express();
+
+app.use('/docs', express.static(path.join(__dirname, '../documentation')));
+
 require('./express')(app);
 require('./routes')(app);
 require('./auth/strategies')();
