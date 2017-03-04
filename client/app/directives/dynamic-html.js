97e91079e2d4b94415f4899b9f8b5370b1b87d6d
ChenCristal <chencristal2014@gmail.com>
2017-03-04 22:14:48 +0800

    removed temporary code for animation

--- client/app/directives/dynamic-html.js
+++ client/app/directives/dynamic-html.js
@@ -39,11 +39,9 @@ angular.module('app')
       scope.$watch(attrs.ngVisible, function(newVal) {        
         if (newVal === true) {
           jQuery(element).show(500);
-          console.log(element);
         }
         else if (newVal === false) {
           jQuery(element).hide(500);
-          console.log(element);
         }
       });
     }
