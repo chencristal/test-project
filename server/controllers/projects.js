2af875874f000eda132e8894edfe488304feb960
Innovation <stardonnie@gmail.com>
2017-03-14 01:19:01 +0800

    newline fix in expandables

--- server/controllers/projects.js
+++ server/controllers/projects.js
@@ -260,16 +260,16 @@ function _getCompiledTemplate(data) {
           if(subs.length > 0) {
             var glue = ' ';
             if(newline && prettify)
-              glue = ',<br/>';
+              glue = ',\n<br/>';
             else if(newline)
-              glue = '<br/>';
+              glue = '\n<br/>';
             else if(prettify)
               glue = ', ';
             if(prettify) {
               var temp = subs.slice(0,-1).join(glue);
               var last = subs[subs.length-1];
               if(newline)
-                subs = temp + '<br/>and ' + last;
+                subs = temp + ' and\n<br/>' + last;
               else
                 subs = temp + ' and ' + last;
             }
