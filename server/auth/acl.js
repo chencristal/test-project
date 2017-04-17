af9c652d72e3e250fca6ea3efc40425924a33cb4
ChenCristal <chencristal2014@gmail.com>
2017-04-18 03:53:37 +0800

    fixed should is not working issue

--- server/auth/acl.js
+++ server/auth/acl.js
@@ -6,96 +6,7 @@ var ModuleAcl      = require('acl');
 var customErrors   = require('n-custom-errors');
 
 var acl = null;
-
-exports.initialize = function(connection) {  
-
-  if (acl !== null) {
-    return false;
-  }
-
-  acl = new ModuleAcl(new ModuleAcl.mongodbBackend(connection.db, 'acl_'));
-  exports.acl = acl;
-  initializeUserRoles();
-};
-
-exports.removeUser = function(user) {
-  var roles = ['superadmin', 'admin', 'author', 'user'];
-  return new Promise((resolve, reject) => {
-    acl.removeUserRoles(user.email, roles, function(err) {
-      if (err) { reject(err); }
-      
-      resolve(user);
-    });
-  });
-};
-
-exports.userRoles = function(user) {
-  return new Promise((resolve, reject) => {
-    acl.userRoles(user.email, function(err, roles) {
-      if (err) {
-        reject(err);
-      } 
-      else {
-        if (_.isEmpty(roles)) {
-          exports.addUserToAcl(user)
-            .then(user => resolve([user.role]))
-            .catch(err => { 
-              reject(err); 
-            });
-        }
-        else {
-          resolve(roles);
-        }
-      }
-    });
-  });
-};
-
-exports.isAllowed = function(user, resource, action) {
-  return new Promise((resolve, reject) => {
-    acl.isAllowed(user.email, resource, action, function(err, allowed) {
-      if (err) { reject(err); }
-      
-      if (allowed === true) { resolve(user); }
-      
-      resolve(false);
-    });
-  });
-};
-
-exports.addUserToAcl = function(user) {
-  function removeAllUserRoles(user) {
-    var roles = ['superadmin', 'admin', 'author', 'user'];
-    return new Promise((resolve, reject) => {
-      acl.removeUserRoles(user.email, roles, function(err) {
-        if (err) { reject(err); }
-
-        resolve(user);
-      });
-    });
-  }
-
-  function addUserRole(user) {
-    return new Promise((resolve, reject) => {
-      acl.addUserRoles(user.email, user.role, function(err) {
-        if (err) { reject(err); }
-
-        resolve(user);
-      });
-    });
-  }
-
-  return removeAllUserRoles(user)
-    .then(addUserRole)
-    .catch(err => {
-      err = customErrors.getAccessDeniedError('Access denied');
-      return err;
-    });
-};
-
-
-function initializeUserRoles() {
-  return Promise.resolve(() => acl.allow([
+var allowance = [
     {
       roles: ['superadmin'],
       allows: [
@@ -203,5 +114,102 @@ function initializeUserRoles() {
         }
       ]
     }
-  ]));
+  ];
+
+exports.initialize = function(connection) {  
+
+  if (acl !== null) {
+    return false;
+  }
+
+  acl = new ModuleAcl(new ModuleAcl.mongodbBackend(connection.db, 'acl_'));
+  exports.acl = acl;
+
+  return initializeUserRoles();
+};
+
+exports.removeUser = function(user) {
+  var roles = ['superadmin', 'admin', 'author', 'user'];
+  return new Promise((resolve, reject) => {
+    acl.removeUserRoles(user.email, roles, function(err) {
+      if (err) { reject(err); }
+      
+      resolve(user);
+    });
+  });
+};
+
+exports.userRoles = function(user) {
+  return new Promise((resolve, reject) => {
+    acl.userRoles(user.email, function(err, roles) {
+      if (err) {
+        reject(err);
+      } 
+      else {
+        if (_.isEmpty(roles)) {
+          exports.addUserToAcl(user)
+            .then(user => resolve([user.role]))
+            .catch(err => { 
+              reject(err); 
+            });
+        }
+        else {
+          resolve(roles);
+        }
+      }
+    });
+  });
+};
+
+exports.isAllowed = function(user, resource, action) {
+  return new Promise((resolve, reject) => {
+    acl.isAllowed(user.email, resource, action, function(err, allowed) {
+      if (err) { reject(err); }
+      
+      if (allowed === true) { resolve(user); }
+      
+      resolve(false);
+    });
+  });
+};
+
+exports.addUserToAcl = function(user) {
+  function removeAllUserRoles(user) {
+    var roles = ['superadmin', 'admin', 'author', 'user'];
+    return new Promise((resolve, reject) => {
+      acl.removeUserRoles(user.email, roles, function(err) {
+        if (err) { reject(err); }
+
+        resolve(user);
+      });
+    });
+  }
+
+  function addUserRole(user) {
+    return new Promise((resolve, reject) => {
+      acl.addUserRoles(user.email, user.role, function(err) {
+        if (err) { reject(err); }
+
+        resolve(user);
+      });
+    });
+  }
+
+  return removeAllUserRoles(user)
+    .then(addUserRole)
+    .catch(err => {
+      err = customErrors.getAccessDeniedError('Access denied');
+      return err;
+    });
+};
+
+
+function initializeUserRoles() {
+  return new Promise((resolve, reject) => {
+    acl.allow(allowance, function(err) {
+      if (err) { reject(err); }
+
+      resolve(allowance);
+    });
+  });
 }
